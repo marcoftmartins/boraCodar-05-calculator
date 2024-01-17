166541dd504bcc4a4673ed81c8f603a63c323b42
Marco Martins <marcomartins@Marcos-MacBook-Air.local>
2024-01-17 02:24:50 +0000

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,47 @@
+/** @format */
+
+const input = document.querySelector('.result-number')
+const lastCalc = document.querySelector('.last-sum')
+const operators = ['+', '-', '/', '*']
+let ans = false
+
+const getValue = (event) => {
+  if (!operators.includes(event.target.innerText)) {
+    input.innerHTML += event.target.innerText
+  } else {
+    input.innerHTML = ''
+    if (ans) {
+      lastCalc.innerHTML = eval(lastCalc.innerText)
+      ans = false
+    }
+  }
+  lastCalc.innerHTML += '' + event.target.innerText
+}
+
+const erase = () => {
+  input.innerHTML = input.innerText.slice(0, -1)
+}
+
+const clearCalculator = () => {
+  input.innerHTML = ''
+  lastCalc.innerHTML = ''
+}
+
+const calculate = () => {
+  if (input.innerText != '') {
+    input.innerHTML = eval(lastCalc.innerText)
+    ans = true
+  }
+}
+
+const percentage = () => {
+  if (input.innerText != '') {
+    input.innerHTML = parseFloat(input.innerText) / 100
+  }
+}
+
+const convert = () => {
+  if (input.innerText != '') {
+    input.innerHTML *= -1
+  }
+}
